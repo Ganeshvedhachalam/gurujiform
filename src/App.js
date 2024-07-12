685281bbe9c10e477c8dc9e7ae9e5d937d11a064
Ganeshvedhachalam <ganeshgreat08@gmail.com>
2024-07-12 17:33:05 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import Form from './Components/Form';
-
+import { SnackbarProvider } from "notistack";
+import "./App.css";
+import Form from "./Components/Form";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       <Form/>
-      </header>
-    </div>
+    <SnackbarProvider maxSnack={1}>
+      <div className="App">
+        <Form />
+      </div>
+    </SnackbarProvider>
   );
 }
 
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,8 +1,11 @@
-import { useState } from "react";
+import { useSnackbar } from "notistack";
+import { useEffect, useState } from "react";
+import profile from "../../src/assets/guruji-astro-squarelogo.webp";
+import "./form.css";
 
 function Form() {
   const [step, setStep] = useState(1);
-  const [formdata, Setformdata] = useState({
+  const [formData, setFormData] = useState({
     name: "",
     email: "",
     phone: "",
@@ -12,156 +15,233 @@ function Form() {
     state: "",
     zip: "",
   });
+  const { enqueueSnackbar } = useSnackbar();
 
-  const HandleChange = (e) => {
+  // Load form data from local storage on initial render
+  useEffect(() => {
+    const storedData = localStorage.getItem("formData");
+    if (storedData) {
+      setFormData(JSON.parse(storedData));
+    }
+  }, []);
+
+  // Save form data to local storage whenever it changes
+  useEffect(() => {
+    localStorage.setItem("formData", JSON.stringify(formData));
+  }, [formData]);
+
+  const handleChange = (e) => {
     const { name, value } = e.target;
-    Setformdata({ ...formdata, [name]: value });
+    setFormData({ ...formData, [name]: value });
   };
-  const HandleNext = (e) => {
-    setStep((prev) => prev + 1);
+
+  const handleNext = (e) => {
+    
+    if (
+      (step === 1 && formData.name && formData.email && formData.phone) ||
+      (step === 2 &&
+        formData.address1 &&
+        formData.address2 &&
+        formData.state &&
+        formData.city &&
+        formData.zip)
+    ) {
+      e.preventDefault();
+      if (formData.name && formData.email) {
+        if (formData.phone.length !== 10) {
+          enqueueSnackbar("phone number must be 10 digit", {
+            variant: "error",
+          });
+        } else {
+          setStep((prev) => prev + 1);
+          enqueueSnackbar(" next section!", { variant: "info" });
+        }
+      }
+    }
   };
-  const HandlePrev = (e) => {
+
+  const handlePrev = (e) => {
+    e.preventDefault();
     setStep((prev) => prev - 1);
+    enqueueSnackbar(" previous section!", { variant: "info" });
   };
+
   const handleSubmit = (e) => {
-    e.prevent.default();
+    e.preventDefault();
+    // Perform form submission tasks
+    setFormData({
+      name: "",
+      email: "",
+      phone: "",
+      address1: "",
+      address2: "",
+      city: "",
+      state: "",
+      zip: "",
+    });
+    localStorage.removeItem("formData");
+    setStep(1);
+    enqueueSnackbar("Form submitted successfully!", { variant: "success" });
   };
 
   return (
     <>
-      <h1>Guruji Astro</h1>
-      <form onSubmit={""}>
-        {step === 1 && (
-          <div>
-            <h1>Personal Details</h1>
-            <label>Name</label>
-            <input
-              name="name"
-              type="text"
-              value={formdata.name}
-              placeholder=""
-              required
-              onChange={HandleChange}
-            />
-            <br></br>
-            <label>Email</label>
-            <input
-              name="email"
-              type="email"
-              value={formdata.email}
-              placeholder=""
-              required
-              onChange={HandleChange}
-            />
-            <br></br>
-            <label>phone</label>
-            <input
-              name="phone"
-              type="tel"
-              value={formdata.phone}
-              placeholder=""
-              required
-              onChange={HandleChange}
-            />
-            <br></br>
+      <div className="container">
+        <img
+          src={profile}
+          alt="Logo"
+          style={{ marginTop: "5px" }}
+          width={100}
+          height={100}
+        />
+
+        <form onSubmit={handleSubmit}>
+          <h2
+            style={{
+              color: "orange",
+              margin: "5px",
+              textShadow: "1px 0 blue, -1px 0 blue, 0 1px blue, 0 -1px blue",
+            }}
+          >
+            Guruji Astro
+          </h2>
+          {step === 1 && (
+            <div className="info">
+              <h1>Personal Details</h1>
+             <div className="formlab"> <div style={{width:"100px"}}><label>Name</label></div>
+             <div><input
+                name="name"
+                type="text"
+                value={formData.name}
+                placeholder=""
+                required
+                onChange={handleChange}
+              /></div> </div>
+              <br />
+             <div className="formlab"> <div style={{width:"100px"}}><label>Email</label></div>
+             <div><input
+                name="email"
+                type="email"
+                value={formData.email}
+                placeholder=""
+                required
+                onChange={handleChange}
+              /></div> </div>
+              <br />
+             <div className="formlab"> <div style={{width:"100px"}}><label>Phone</label></div>
+             <div><input
+                name="phone"
+                type="text"
+                pattern="[0-9]{10}"
+                value={formData.phone}
+                placeholder=""
+                required
+                onChange={handleChange}
+              /></div> </div>
+              <br />
+            </div>
+          )}
+
+          <br />
+
+          {step === 2 && (
+            <div className="address">
+              <h2>Address Information</h2>
+             <div className="formlab"> <div style={{width:"150px"}}><label>Address Line 1</label></div>
+             <div><input
+                name="address1"
+                type="text"
+                value={formData.address1}
+                placeholder=""
+                required
+                onChange={handleChange}
+              /></div> </div>
+              <br />
+             <div className="formlab"> <div style={{width:"150px"}}><label>Address Line 2</label></div>
+             <div><input
+                name="address2"
+                type="text"
+                value={formData.address2}
+                placeholder=""
+                required
+                onChange={handleChange}
+              /></div> </div>
+              <br />
+             <div className="formlab"> <div style={{width:"150px"}}><label>City</label></div>
+             <div><input
+                name="city"
+                type="text"
+                value={formData.city}
+                placeholder=""
+                required
+                onChange={handleChange}
+              /></div> </div>
+              <br />
+             <div className="formlab"> <div style={{width:"150px"}}><label>State</label></div>
+             <div><input
+                name="state"
+                type="text"
+                value={formData.state}
+                placeholder=""
+                required
+                onChange={handleChange}
+              /></div> </div>
+              <br />
+             <div className="formlab"> <div style={{width:"150px"}}><label>Zip Code</label></div>
+             <div><input
+                name="zip"
+                type="number"
+                value={formData.zip}
+                placeholder=""
+                required
+                onChange={handleChange}
+              /></div> </div>
+              <br />
+            </div>
+          )}
+
+          {step === 3 && (
+            <div   className="confirmation">
+              <h1 style={{}}>Confirmation</h1>
+              <p>
+                <strong>Name : </strong> {formData.name}</p>
+              <p>
+                <strong>Email : </strong> {formData.email}</p>
+              <p>
+                <strong>Phone : </strong> {formData.phone}</p>
+              <p>
+                <strong>Address1 : </strong> {formData.address1}</p>
+              <p>
+                <strong>Address2 : </strong> {formData.address2}</p>
+              <p>
+                <strong>City : </strong> {formData.city}</p>
+              <p>
+                <strong>State : </strong> {formData.state}</p>
+              <p>
+                <strong>Zip : </strong> {formData.zip}</p>
+            </div>
+          )}
+
+          <div className="buttons">
+            <button onClick={handlePrev} disabled={step === 1}>Back</button>
+            {step < 3 && <button onClick={handleNext}>Next</button>}
+            {step === 3 && <button type="submit">Submit</button>}
           </div>
-        )}
-
-        <br></br>
-
-        {step === 2 && (
-          <div>
-            <h2>Address Information </h2>
-            <label>Address Line 1 </label>
-            <input
-              name="address1"
-              type=""
-              value={formdata.address1}
-              placeholder=""
-              required
-              onChange={HandleChange}
-            />
-            <br></br>
-            <label>Address Line 2 </label>
-            <input
-              name="address2"
-              type=""
-              value={formdata.address2}
-              placeholder=""
-              required
-              onChange={HandleChange}
-            />
-            <br></br>
-            <label>City</label>
-            <input
-              name="city"
-              type="text"
-              value={formdata.city}
-              placeholder=""
-              required
-              onChange={HandleChange}
-            />
-            <br></br>
-            <label>State</label>
-            <input
-              name="state"
-              type="text"
-              value={formdata.state}
-              placeholder=""
-              required
-              onChange={HandleChange}
-            />
-            <br></br>
-            <label>Zip Code</label>
-            <input
-              name="zip"
-              type="number"
-              value={formdata.zip}
-              placeholder=""
-              required
-              onChange={HandleChange}
-            />
-            <br></br>
-          </div>
-        )}
-
-        {step === 3 && (
-          <div>
-            <h1>Confirmation</h1>
-            <p>
-              <strong>Name</strong> {formdata.name}{" "}
-            </p>
-            <p>
-              <strong>Email</strong> {formdata.email}{" "}
-            </p>
-            <p>
-              <strong>Phone</strong> {formdata.phone}{" "}
-            </p>
-            <p>
-              <strong>Address1</strong> {formdata.address1}{" "}
-            </p>
-            <p>
-              <strong>Address2</strong> {formdata.address2}{" "}
-            </p>
-            <p>
-              <strong>City</strong> {formdata.city}{" "}
-            </p>
-            <p>
-              <strong>State</strong> {formdata.state}{" "}
-            </p>
-            <p>
-              <strong>Zip</strong> {formdata.zip}{" "}
-            </p>
-          </div>
-        )}
-
-        <div>
-          {step > 1 && <button onClick={HandlePrev}>back</button>}
-          {step < 3 && <button onClick={HandleNext}>next</button>}
-          {step === 3 && <button onClick={handleSubmit}>submit</button>}
-        </div>
-      </form>
+         {/* < div className="buttons">
+          <button type="button" onClick={handlePrev} disabled={step === 1}>
+            Back
+          </button>
+          {step < 3 && (
+            <button type="button" onClick={handleNext}>
+              Next
+            </button>
+          )}
+          {step === 3 && <button type="submit">Submit</button>}
+        </div> */}
+        </form>
+      </div>
     </>
   );
 }
+
 export default Form;
